66d30160c026ba69649ce5fab3465231ec2f47d1
Gerardo Meza <mezagg@gmail.com>
2019-08-02 00:41:42 -0500

    se agrega nickname

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Usuario } from 'src/models/usuario.model';
-import { UsuarioService } from 'src/app/services/service.index';
+import {Component, OnInit} from '@angular/core';
+import {Usuario} from 'src/models/usuario.model';
+import {UsuarioService} from 'src/app/services/service.index';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -13,51 +13,47 @@ export class ProfileComponent implements OnInit {
   usuario: Usuario;
   imagenSubir: File;
   imagenTemp: string;
-  constructor
-  (
-    public _usuarioService: UsuarioService)
-    {
-      this.usuario = _usuarioService.usuario;
-    }
+
+  constructor(public _usuarioService: UsuarioService) {
+    this.usuario = _usuarioService.usuario;
+  }
 
   ngOnInit() {
   }
-  guardar(usuario:Usuario)
-  {
-    this.usuario.nombre= usuario.nombre;
-    if (!this.usuario.google)
-    {
-      this.usuario.email= usuario.email;
-    } 
+
+  guardar(usuario: Usuario) {
+    this.usuario.nombre = usuario.nombre;
+    this.usuario.nickname = usuario.nickname;
+
+    if (!this.usuario.google) {
+      this.usuario.email = usuario.email;
+    }
     this._usuarioService.actualizarUsuario(this.usuario)
-    .subscribe(resp=>
-      {
+      .subscribe(resp => {
         console.log(resp);
-      })
+      });
   }
-  seleccionImagen(archivo:File)
-  {
-    if (!archivo)
-    {
-      this.imagenSubir=null;
+
+  seleccionImagen(archivo: File) {
+    if (!archivo) {
+      this.imagenSubir = null;
       return;
     }
-    if (archivo.type.indexOf('image')<0)
-    {
-      Swal.fire ('Solo Imagenes','El Archivo seleccionado no es una imagen','error');
-      this.imagenSubir=null;
+    if (archivo.type.indexOf('image') < 0) {
+      Swal.fire('Solo Imagenes', 'El Archivo seleccionado no es una imagen', 'error');
+      this.imagenSubir = null;
       return;
     }
 
-    this.imagenSubir= archivo;
+    this.imagenSubir = archivo;
 
     let reader = new FileReader();
     let urlImagenTemp = reader.readAsDataURL(archivo);
     reader.onloadend = () => this.imagenTemp = reader.result.toString();
-  
+
   }
-  cambiarImagen()
-  {
+
+  cambiarImagen() {
     this._usuarioService.cambiarImagen(this.imagenSubir, this.usuario._id);
   }
 }
