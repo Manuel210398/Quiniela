fcb29a5545c8b92f5558ebbfe6863a67e0748710
Jose Manuel <dx_210398@hotmail.com>
2019-08-05 09:50:25 -0500

    se agrego filtro de jornadas

--- src/app/pages/jornadas/jornadas.component.ts
+++ src/app/pages/jornadas/jornadas.component.ts
@@ -5,6 +5,8 @@ import { EquiposService } from 'src/app/services/equipos/equipos.service';
 import { PartidoService } from 'src/app/services/partidos/partido.service';
 import { Jornada } from 'src/models/jornada.model';
 import { JornadaService } from 'src/app/services/jornadas/jornada.service';
+import { TorneosService } from 'src/app/services/torneos/torneos.service';
+import { Torneo } from 'src/models/torneo.model';
 @Component({
   selector: 'app-jornadas',
   templateUrl: './jornadas.component.html',
@@ -12,10 +14,13 @@ import { JornadaService } from 'src/app/services/jornadas/jornada.service';
 })
 export class JornadasComponent implements OnInit {
   jordanas:Jornada[]=[];
-  constructor(public _EquipoService: EquiposService, public _partidoService: PartidoService, public _jornadaService: JornadaService) { }
+  torneos:Torneo[]=[];
+  constructor(public _EquipoService: EquiposService, public _partidoService: PartidoService, 
+    public _jornadaService: JornadaService, public _torneoService: TorneosService) { }
 
   ngOnInit() {
     this.cargarJornadas();
+    this.cargarTorneos();
   }
   cargarJornadas()
   {
@@ -26,7 +31,29 @@ export class JornadasComponent implements OnInit {
       console.log(this.jordanas);
     });
   }
-  
-  
-
+  cargarTorneos()
+  {
+    this._torneoService.obtenerTorneos()
+    .subscribe((resp:any)=>
+    {
+      this.torneos= resp.torneos;
+      console.log(this.torneos);
+    });
+  }
+  cambioTorneo(event) {
+    console.log(event);
+    this._jornadaService.obtenerJornadasPorPartido(event)
+    .subscribe((resp:any)=>
+    {
+      this.jordanas=resp.jornadas;
+      console.log(this.jordanas);
+    });
+  }
+  eliminarJornada(jornada:Jornada){
+    this._jornadaService.eliminarJornada(jornada._id)
+    .subscribe(()=>{
+      this.cargarJornadas();
+      Swal.fire('Importante', 'La Jornada se ha Eliminado Correctamente', 'success');
+    });
+  }
 }
--- src/app/services/jornadas/jornada.service.ts
+++ src/app/services/jornadas/jornada.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { URL_SERVICIOS } from 'src/app/config/config';
+import { Jornada } from 'src/models/jornada.model';
+import { UsuarioService } from '../usuario/usuario.service';
+import Swal from 'sweetalert2';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +13,47 @@ import { URL_SERVICIOS } from 'src/app/config/config';
 export class JornadaService {
 
   constructor(public http: HttpClient,
-    public router: Router) { }
+    public router: Router,public _usuarioService: UsuarioService) { }
 
 
   obtenerJornada(id:string)
   {
     let url = URL_SERVICIOS + '/jornada/' + id;
     return this.http.get(url);
+  }
+  crearJornada(jornada:Jornada)
+  {
+    let url = URL_SERVICIOS + '/jornada';
+    if (jornada._id){
+      url+='/'+jornada._id;
+      url += '?token=' + this._usuarioService.token;
+      return this.http.put(url,jornada)
+      .pipe(map((resp:any)=>
+        {
+          Swal.fire('Importante', 'La Jornada se ha Actualizado Correctamente', 'success');
+          console.log(resp);
+          return resp;
+        }));
+    }
+    else{
+      url += '?token=' + this._usuarioService.token;
+      return this.http.post(url, jornada)
+        .pipe(map((resp: any) => {
+          Swal.fire('Importante', 'La Jornada se ha Registrado Correctamente', 'success');
+          return resp;
+        }));
+    }
+
+  }
+  obtenerJornadasPorPartido(id:string)
+  {
+    let url = URL_SERVICIOS + '/jornada/torneo/'+ id;
+    return this.http.get(url);
+  }
+  eliminarJornada(id:string)
+  {
+    let url = URL_SERVICIOS + '/jornada/'+ id;
+    url += '?token=' + this._usuarioService.token;
+    return this.http.delete(url);
   }
 }
